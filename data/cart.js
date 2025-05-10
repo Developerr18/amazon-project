d3f2986de4ee5bf2deb6748c7307d109baf21353
Developerr18 <dineshnikum1@gmail.com>
2025-05-10 18:35:52 +0530

    generate html for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,17 @@
-export const cart = [];
+export const cart = [
+    {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+    },
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+    },
+];
 
 // update cart
 export function addToCart(productId, selectedQuantity) {
-    const matchingItem = cart.forEach(
+    const matchingItem = cart.find(
         (cartItem) => cartItem.productId === productId
     );
     if (matchingItem) {
